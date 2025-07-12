7ca86149d95fe826e6a9b5d65ae2d1b7f90ba5a7
Meriemchm <chamimeriem2302@gmail.com>
2025-07-12 18:03:58 +0100

    add*foulish

--- src/components/Routine/RoutineItem.jsx
+++ src/components/Routine/RoutineItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { routineData } from "../Data/Data";
+import flourish from "../../assets/Icons/flourish.svg";
 export const RoutineItem = () => {
   return (
     <div className="flex flex-col md:flex-row gap-4 justify-center items-center ">
@@ -20,8 +21,20 @@ export const RoutineItem = () => {
                 className="w-full h-full object-cover "
               />
             </div>
-            <div className={` ${item.bgColor} py-3 text-center`}>
-              <p className="text-[#5D3FD3] font-medium">{item.name}</p>
+            <div
+              className={`${item.bgColor} relative py-3 flex flex-col items-center justify-center`}
+            >
+              {/* SVG décoratif positionné absolument en haut */}
+              <img
+                src={flourish}
+                alt="flourish"
+                className="absolute top-0 left-1/2 transform -translate-x-1/2"
+              />
+
+              {/* Texte centré */}
+              <p className="text-[#5D3FD3] font-medium relative z-10 mt-6">
+                {item.name}
+              </p>
             </div>
           </div>
         </div>
